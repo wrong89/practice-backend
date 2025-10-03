0654471ccc4de70b202b46af1a330b9aa8a606b6
wrong89 <fenomen_03@mail.ru>
2025-10-04 02:58:46 +0300

    added generic idList and entryList which uses idList

---
+++ internal/storage/inmem/entryList.go
@@ -0,0 +1,80 @@
+package inmem
+
+import (
+	"context"
+	"errors"
+	"practice-backend/internal/models/entry"
+	"practice-backend/internal/storage/inmem/ilist"
+	"sync"
+	"time"
+)
+
+var (
+	ErrEntryNotFound = errors.New("entry not found")
+)
+
+// Concurrent-Use
+type EntryList struct {
+	list ilist.List[entry.Entry]
+	mtx  *sync.Mutex
+}
+
+func NewEntryList() EntryList {
+	return EntryList{
+		list: ilist.NewList[entry.Entry](),
+		mtx:  new(sync.Mutex),
+	}
+}
+
+func (el *EntryList) GetEntryByID(ctx context.Context, id int) (entry.Entry, error) {
+	el.mtx.Lock()
+	defer el.mtx.Unlock()
+
+	e, err := el.list.GetData(id)
+	if err != nil {
+		if errors.Is(err, ilist.ErrDataNotFound) {
+			return entry.Entry{}, ErrEntryNotFound
+		}
+		return entry.Entry{}, err
+	}
+	return e, nil
+}
+
+func (el *EntryList) CreateEntry(
+	ctx context.Context,
+	course string,
+	date time.Time,
+	userID int,
+	paymentMethod string,
+) (entry.Entry, error) {
+	newEntry := entry.NewEntry(course, date, userID, paymentMethod)
+
+	el.mtx.Lock()
+	defer el.mtx.Unlock()
+
+	e, err := el.list.AddData(*newEntry)
+	if err != nil {
+		return entry.Entry{}, err
+	}
+
+	return e, nil
+}
+
+func (el *EntryList) DeleteEntry(ctx context.Context, id int) error {
+	el.mtx.Lock()
+	defer el.mtx.Unlock()
+
+	e, err := el.list.GetData(id)
+	if err != nil {
+		return err
+	}
+
+	if err := el.list.DeleteData(e.ID); err != nil {
+		if errors.Is(err, ilist.ErrDataNotFound) {
+			return ErrEntryNotFound
+		}
+		return err
+	}
+
+	return nil
+}
---
+++ internal/storage/inmem/ilist/ilist.go
@@ -0,0 +1,50 @@
+package ilist
+
+import (
+	"errors"
+)
+
+var (
+	ErrDataAlreadyExist = errors.New("data already exist")
+	ErrDataNotFound     = errors.New("data not found")
+)
+
+type List[V any] struct {
+	list   map[int]V
+	lastID int
+}
+
+func NewList[V any]() List[V] {
+	return List[V]{
+		list:   make(map[int]V),
+		lastID: 0,
+	}
+}
+
+func (l *List[V]) GetData(id int) (V, error) {
+	result, ok := l.list[id]
+	if !ok {
+		return *new(V), ErrDataNotFound
+	}
+
+	return result, nil
+}
+
+func (l *List[V]) AddData(data V) (V, error) {
+	if _, err := l.GetData(l.lastID + 1); err == nil {
+		return *new(V), ErrDataAlreadyExist
+	}
+	l.lastID++
+	l.list[l.lastID] = data
+
+	return data, nil
+}
+
+func (l *List[V]) DeleteData(id int) error {
+	if _, err := l.GetData(id); err != nil {
+		return err
+	}
+
+	delete(l.list, id)
+	return nil
+}
